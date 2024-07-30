795c581ad8d5272cd277f9b903f7213a35493535
jenil0566 <jenilgupta8@gmail.com>
2024-07-30 07:39:53 +0530

    Update ProductList.jsx

--- src/Pages/Admin/ProductList.jsx
+++ src/Pages/Admin/ProductList.jsx
@@ -99,7 +99,7 @@ console.log(products)
             const token = localStorage.getItem('token')
 
             const id = e
-            const response = await axios.delete(`http://157.245.105.187/api/products/${e}`,{ headers: {
+            const response = await axios.delete(`${import.meta.env.VITE_BASE_URL}/api/products/${e}`,{ headers: {
                 'Content-Type': 'multipart/form-data',
                 'Authorization': `Bearer ${token}`
             }})
